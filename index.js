fb6a59c800cadbcdcdc1e5dce46bdd4853d1de77
Mahesh <kmaheshgupta151@outlook.com>
2022-11-27 12:55:39 +0530

    make it better

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const router = require("./Brands/Router")
-
-
+const dotenv  = require("dotenv")
+dotenv.config()
 const app = express()
 
 app.use(cors())
@@ -15,6 +15,6 @@ app.get("/", function(req, res) {
   //when we get an http get request to the root/homepage
   res.send("Hello World");
 });
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5003,()=>{
       console.log("App Listening on port 5000!!!")
 })
